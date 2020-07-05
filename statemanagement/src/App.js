1614b288c5cb309383d4ca7f9a3cc4de392394e9
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-06 02:36:07 +0530

    Created a 'AddMovie' Component to add movies to the context API states
    Styled the form with lighter theme.
    Added the component to the App component to make use of the MovieProvider Context API.

--- statemanagement/src/App.js
+++ statemanagement/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import MovieList from "./components/MovieList";
 import Nav from "./components/Nav";
 import { MovieProvider } from "./contexts/MovieContext";
+import AddMovie from "./components/AddMovie";
 
 function App() {
 	return (
     <MovieProvider>
       <div>
         <Nav />
+        <AddMovie />
         <MovieList />
       </div>
     </MovieProvider>
---
+++ statemanagement/src/components/AddMovie.js
@@ -0,0 +1,61 @@
+import React, { useState, useContext } from "react";
+import { MovieContext } from "../contexts/MovieContext";
+
+const AddMovie = () => {
+	const [movies, setMovies] = useContext(MovieContext);
+	const [formData, setFormData] = useState({
+		name: "",
+		price: "",
+		id: movies[movies.length - 1].id + 1,
+	});
+
+	//destructuring
+	const { name, price, id } = formData;
+
+	const handleChange = (fieldName) => (e) => {
+		setFormData({ ...formData, [fieldName]: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setMovies([...movies, formData]);
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<label htmlFor="name">Movie Name</label>
+			<input
+				type="text"
+				name="name"
+				id="name"
+				value={name}
+				onChange={handleChange("name")}
+				required={true}
+			/>
+			<br />
+			<label htmlFor="price">Price</label>
+			<input
+				type="number"
+				name="price"
+				id="price"
+				value={price}
+				onChange={handleChange("price")}
+				required={true}
+			/>
+			<br />
+			<label htmlFor="id">ID</label>
+			<input
+				type="number"
+				name="id"
+				id="id"
+				value={id}
+				disabled={true}
+				required={true}
+			/>
+			<br />
+			<button>Create Movie</button>
+		</form>
+	);
+};
+
+export default AddMovie;
